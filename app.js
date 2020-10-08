eecd5bdb8b4876cc3c5f3fadf022da0876fc7846
RQuicho <raymondquicho@gmail.com>
2020-10-07 17:07:22 -0700

    Using ejs for website

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+  res.render("home.ejs");
+});
+
+app.get("/about", function(req, res) {
+  res.render("about.ejs");
+});
+
+app.get("/poetry", function(req, res) {
+  res.render("poetry.ejs");
+});
+
+app.get("/articles", function(req, res) {
+  res.render("articles.ejs");
+});
+
+app.get("/essays", function(req, res) {
+  res.render("essays.ejs");
+});
+
+app.get("/contact", function(req, res) {
+  res.render("contact.ejs");
+});
+
+app.get("/404", function(req, res) {
+  res.render("404.ejs");
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
